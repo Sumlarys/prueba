666ebe8173ca05e55dea8052d5c3da01c10ce25d
Ren <projectren13@gmail.com>
2023-05-16 18:55:24 +0200

    Algo

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package prueba;
+
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Bye World!");
+        System.out.println("Bye World!");
+    }
+}
